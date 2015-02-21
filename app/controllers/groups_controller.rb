03a3d2ed32cf2576d7852de1cf1ffb8f46762d99
przemkow <przemkow92@gmail.com>
2015-02-21 15:14:55 +0100

    added ability to add user to group, layouch changes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,15 +20,31 @@ class GroupsController < ApplicationController
   end
 
   def add_user
+    @group = Group.find(params[:id])
     @user = User.new
   end
 
   def create_user
+    @group = Group.find(params[:id])
+    @user = User.find_by email: user_params[:email]
+    if @user && !(@group.users.include?(@user))
+      @group.users << @user
+      redirect_to group_path(@group)
+    else
+      @user = User.new(user_params)
+      @user.errors.add(:email, 'is not in database')
+      render :add_user
+    end
   end
 
   private
+
     def group_params
       params.require(:group).permit(:name)
+    end
+
+    def user_params
+      params.require(:user).permit(:email)
     end
 
     def authenticate_user_in_group
@@ -37,3 +53,4 @@ class GroupsController < ApplicationController
       end
     end
 end
+
