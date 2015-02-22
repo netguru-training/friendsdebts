c0a7ada999550d68ea4685bfef9e526ff8e6312e
przemkow <przemkow92@gmail.com>
2015-02-22 15:26:57 +0100

    fill in controller spec

---
+++ spec/controllers/groups_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe API::V1::GroupsController, type: :controller do
+
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "user_#{n}@mail.com" }
+    password "12345678"
   end
 
 end
