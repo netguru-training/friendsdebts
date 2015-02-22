d6719806f1ed3af51e9e7a8ffc76088c54c408e7
przemkow <przemkow92@gmail.com>
2015-02-22 12:28:00 +0100

    add Omniauth controller and rest of facebook sing up configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,21 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
   attr_accessor :amount
 
   has_many :groups, through: :memberships
   has_many :memberships
   has_many :recipes
   has_many :recipe_members
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
